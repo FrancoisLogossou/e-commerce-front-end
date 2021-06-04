ec1cc77ce1d1918fb0b34d2360065c9828480aa0
Francois <francoislogossou@orange.fr>
2021-06-04 14:40:01 +0200

    François: création des composants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdresseComponent } from './components/adresse/adresse.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { HomeComponent } from './components/home/home.component';
+import { LivreComponent } from './components/livre/livre.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PersonneComponent } from './components/personne/personne.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "adresse", component: AdresseComponent },
+  { path: "connexion", component: ConnexionComponent },
+  { path: "home", component: HomeComponent },
+  { path: "livre", component: LivreComponent },
+  { path: "menu", component: MenuComponent },
+  { path: "personne", component: PersonneComponent},
+  { path: "not-found", component: NotFoundComponent},
+  { path: "", redirectTo: "/home", pathMatch: "full"},
+  { path: "**", redirectTo: "/not-found"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
